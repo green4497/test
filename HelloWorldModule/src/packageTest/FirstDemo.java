e829151d3123f7c5fbcc90f44c3decaf443acfab
green4497 <dick_ben@163.com>
2022-02-07 18:26:16 +0800

    test2022020702

--- HelloWorldModule/src/packageTest/FirstDemo.java
+++ HelloWorldModule/src/packageTest/FirstDemo.java
@@ -1,7 +1,13 @@
 package packageTest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FirstDemo {
     public static void main(String[] args){
+        //syout
+        ArrayList al=new ArrayList();
+        al.add("1");
         System.out.println("HelloWorld");
     }
 }
