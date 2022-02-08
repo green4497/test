5a9b96f28956eed286535d94ea4ca1ed056369f6
green4497 <dick_ben@163.com>
2022-02-08 10:32:23 +0800

    test20220208

--- HelloWorldModule/src/packageTest/FirstDemo.java
+++ HelloWorldModule/src/packageTest/FirstDemo.java
@@ -1,13 +1,15 @@
 package packageTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class FirstDemo {
     public static void main(String[] args){
-        //syout
+        //sout
         ArrayList al=new ArrayList();
         al.add("1");
+        System.out.println("al = " + al.get(0));
         System.out.println("HelloWorld");
     }
 }
